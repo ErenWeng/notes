5a7faf8734ed6ea3e2f10ed56ca71e3fcf997d1b
eren <f245986@gmail.com>
2023-09-09 11:43:01 +0800

    2023-09-09 11:43:01

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,8 +23,8 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh-tw',
+    locales: ['zh-tw'],
   },
 
   presets: [
@@ -72,7 +72,7 @@ const config = {
           //   position: 'left'
           // },
           {
-            href: 'https://github.com/ErenWeng',
+            href: 'https://github.com/ErenWeng/notes',
             label: 'GitHub',
             position: 'right',
           },
